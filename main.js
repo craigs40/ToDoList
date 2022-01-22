ef3ea6c052197c30092e546ee325963155a2c4d5
Shay Craig <71286979+craigs40@users.noreply.github.com>
2022-01-21 20:01:56 -0500

    Fix linter errors

--- main.js
+++ main.js
@@ -3,15 +3,15 @@ const itemsContainer = document.getElementById('itemsContainer');
 const inputField = document.getElementById('inputField');
 
 addButton.addEventListener('click', () => {
-    var paragraph = document.createElement('p');
-    paragraph.classList.add('p-style');
-    paragraph.innerText = inputField.value;
-    itemsContainer.appendChild(paragraph);
-    inputField.value = '';
-    paragraph.addEventListener('click', () => {
-        paragraph.style.textDecoration = "line-through";
-    })
-    paragraph.addEventListener('dblclick', () => {
-        itemsContainer.removeChild(paragraph);
-    });
+  const paragraph = document.createElement('p');
+  paragraph.classList.add('p-style');
+  paragraph.innerText = inputField.value;
+  itemsContainer.appendChild(paragraph);
+  inputField.value = '';
+  paragraph.addEventListener('click', () => {
+    paragraph.style.textDecoration = 'line-through';
+  });
+  paragraph.addEventListener('dblclick', () => {
+    itemsContainer.removeChild(paragraph);
+  });
 });
